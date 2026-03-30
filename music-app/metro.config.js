667feaabb0ea5f592f5e78eb14f5b007ec79151f
Chaisit91 <Jorvinbasla@gmail.com>
2026-03-30 10:16:34 +0700

    Update metro.config.js

--- music-app/metro.config.js
+++ music-app/metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Fix: reselect (used by Redux Toolkit) ships .mjs files — Metro needs to know about them
+config.resolver.sourceExts = [...config.resolver.sourceExts, "mjs"];
+
 module.exports = withNativeWind(config, { input: "./global.css" });
