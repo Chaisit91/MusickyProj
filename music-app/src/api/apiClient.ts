d6d39e23ba169585e7fecaeb220fa75676ba2702
Chaisit91 <Jorvinbasla@gmail.com>
2026-04-10 10:04:36 +0700

    Update apiClient.ts

--- music-app/src/api/apiClient.ts
+++ music-app/src/api/apiClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const getBaseUrl = () => {
-  return "http://192.168.1.63:8080/api";
+  return "http://172.22.24.207:8080/api";
 };
 
 const BASE_URL = getBaseUrl();
